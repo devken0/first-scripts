4ab097bdf8561dad946a6e482e0fc084bd96ae17
ken <ken@minihomebox.lan>
2024-03-18 18:40:04 +0800

    Update ubuntu-server.sh

--- on-premise/ubuntu-server.sh
+++ on-premise/ubuntu-server.sh
@@ -79,6 +79,70 @@ nextcloud_snap_setup(){
     #sudo snap set nextcloud http.notify-push-reverse-proxy=true
     #sudo snap set nextcloud nextcloud.cron-interval=10m
     #sudo nextcloud.enable-https lets-encrypt
+    # backup except data
+    #sudo rm -rf /var/snap/nextcloud/common/nextcloud/backups
+    #sudo mkdir /mnt/slow_2tb/nc-backups
+    #sudo ln -s /mnt/slow_2tb/nc-backups /var/snap/nextcloud/common/nextcloud/backups
+    #sudo nextcloud.export -abc
+    # restoration
+    #sudo rm -rf /var/snap/nextcloud/common/nextcloud/backups
+    #sudo ln -s /mnt/slow_2tb/nc-backups /var/snap/nextcloud/common/nextcloud/backups
+    #sudo nextcloud.import -abc /var/snap/nextcloud/common/nextcloud/backups/file_name
+#!/usr/bin/env bash
+##############################################################
+## Nextcloud snap backup with Snap snapshots -- SCUBA --
+## -scubamuc- https://scubamuc.github.io/ 
+##############################################################
+## create target directory "sudo mkdir /media/SNAPBACKUP"
+## snapshot rotation 30 days 
+## create crontab as root for automation
+## 0 1 * * 0 su $USER /home/$USER/bin/snapsnapshot.sh
+##############################################################
+# VARIABLES #
+##############################################################
+
+SNAPNAME="nextcloud"
+TARGET="/media/SNAPBACKUP"  ## target directory
+LOG="/media/SNAPBACKUP/snapbackup-nc.log"  ## logfile
+SOURCE="/var/lib/snapd/snapshots" ## source directory
+RETENTION="30" ## retention in days
+
+##############################################################
+# FUNCTIONS #
+##############################################################
+
+## Timestamp for Log ##
+timestamp()
+{
+ date +"%Y-%m-%d %T"
+}
+
+##############################################################
+# SCRIPT #
+##############################################################
+
+## start log  
+echo "********************************************************" >> "$LOG" ; ## log seperator
+echo "$(timestamp) -- Snapbackup $SNAPNAME Start" >> "$LOG" ; ## start log
+
+## stop snap for snapshot  
+ sudo snap stop "$SNAPNAME" ;
+## create snap snapshot 
+ sudo snap save --abs-time "$SNAPNAME" ;
+## restart snap after snapshot 
+ sudo snap start "$SNAPNAME" ;
+
+## find and move snapshot to $TARGET  
+ sudo find "$SOURCE"/ -name "*.zip" -exec mv '{}' "$TARGET"/ \; # find and move
+## find old snapshots and delete snapshots older than $RETENTION days
+ sudo find "$TARGET"/ -name "*.zip" -mtime +"$RETENTION" -exec rm -f {} \; # find and delete
+
+## end log 
+ echo "$(timestamp) -- Snapbackup "$SNAPNAME" End " >> "$LOG" ; ## end log 
+ echo "" >> "$LOG" ;  ## log linefeed 
+
+exit
+
 }
 
 #rclone_setup(){
